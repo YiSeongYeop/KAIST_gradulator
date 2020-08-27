cedec2907f9254b636e4926124468d98789722cf
SeongYeop <appleminto55@gmail.com>
2020-08-27 19:18:46 +0900

    explanatory note: grade condition

--- KAIST_gradulator.py
+++ KAIST_gradulator.py
@@ -1,4 +1,18 @@
 '''
 This explanatory note is about KAIST Graduation Conditions
 
+Grade(School Year)
+	if 34 >= credits : 1 <revision 2015. 10. 26.>
+	if 35 <= credits <= 68 : 2 <revision 2015. 10. 26.>
+	if 69 <= credits <= 102 : 3 <revision 2015. 10. 26.>
+	if 103 <= credits : 4 <revision 2015. 10. 26.>
+
+	1 학년 : 34학점을 취득할 때까지 <개정 2015. 10. 26.>
+	2 학년 : 35학점 이상 68학점을 취득할 때까지 <개정 2015. 10. 26.>
+	3 학년 : 69학점 이상 102학점을 취득할 때까지 <개정 2015. 10. 26.>
+	4 학년 : 103학점 이상 취득하였을 때 <개정 2015. 10. 26.>
+
+Departments:
+	
+
 '''
