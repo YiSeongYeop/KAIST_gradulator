50c5af36a739b82eee255a2539622b12deb7f147
SeongYeop <appleminto55@gmail.com>
2020-08-28 21:52:00 +0900

    for saving

--- calculate.js
+++ calculate.js
@@ -1,5 +1,13 @@
-/*
-$(document).ready(function() {
-    $('#test2').addClass('up');
-});
-*/
+function excelExport(event){
+    var input = event.target;
+    var reader = new FileReader();
+    reader.onload = function(){
+        var fileData = reader.result;
+        var wb = XLSX.read(fileData, {type : 'binary'});
+        wb.SheetNames.forEach(function(sheetName){
+	        var rowObj =XLSX.utils.sheet_to_json(wb.Sheets[sheetName]);
+	        console.log(JSON.stringify(rowObj));
+        })
+    };
+    reader.readAsBinaryString(input.files[0]);
+}
