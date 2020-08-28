09ba46be28b3cec418f8faeb894150f7405c1598
SeongYeop <appleminto55@gmail.com>
2020-08-28 20:13:59 +0900

    for play: login complete

--- login.js
+++ login.js
@@ -1,8 +1,7 @@
-String id = document.getElementById("ID_input");
-String pw = document.getElementById("PW_input");
-
 function login_check() {
-  if (id == "aaa") && (pw == "aaa") {
+  let id = document.getElementById("ID_input").value;
+  let pw = document.getElementById("PW_input").value;
+  if ((id == "aaa") && (pw == "aaa")) {
     window.location.href = "file:///C:/Users/닭다리/Desktop/KAIST_gradulator/calculate.html";
   }
   else {
