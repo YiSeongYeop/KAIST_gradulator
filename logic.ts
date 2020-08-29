d69e2462230fe2c74f18d87d51692b58b2f31ab8
Jihoon Kim <kjh618@kaist.ac.kr>
2020-08-29 23:36:09 +0900

    Add example requirements

--- logic.ts
+++ logic.ts
@@ -1,4 +1,4 @@
-type Department = "(공통)" | "전산학부" | "전기및전자공학부";
+type Department = "전산학부" | "전기및전자공학부" | null;
 
 class Major {
   department: Department;
@@ -7,7 +7,7 @@ class Major {
 class AdvancedMajor {
 }
 
-class IndividuallyDesignedMajor{
+class IndividuallyDesignedMajor {
 }
 
 class Minor {
@@ -18,12 +18,16 @@ class DoubleMajor {
   department: Department;
 }
 
-type CourseClassification = "교양필수" | "교양필수 AU" | "인문사회선택" | "기초필수" | "기초선택" | "전공필수 (전공선택)" | "전공선택" | "연구";
+type Extra = AdvancedMajor | IndividuallyDesignedMajor | Minor | DoubleMajor;
+
+type CourseClassification = "교양필수" | "교양필수 AU" | "인문사회선택" | "기초필수" | "기초선택" | "전공필수" | "전공선택" | "연구";
+
+type CourseNumber = string;
 
 class Course {
   department: Department;
   classification: CourseClassification;
-  number: string;
+  number: CourseNumber;
   code: string;
   name: string;
   credit: number;
@@ -31,28 +35,102 @@ class Course {
 
 class Student {
   major: Major;
-  extra: AdvancedMajor | IndividuallyDesignedMajor | Minor | DoubleMajor;
-  courses: Course[];
+  extra: Extra; // TODO: Change to string literal union + nullable department?
+  courses: CourseNumber[];
 }
 
 abstract class RequiredCourse {
-  credit: number;
 }
 
 class SpecificCourse extends RequiredCourse {
-  course: Course;
+  course: CourseNumber;
+
+  constructor(course: CourseNumber) {
+    super();
+    this.course = course;
+  }
 }
 
 class ChooseNum extends RequiredCourse {
   num: number;
-  courses: Course[];
+  courses: CourseNumber[];
+
+  constructor(num: number, courses: CourseNumber[]) {
+    super();
+    this.num = num;
+    this.courses = courses;
+  }
 }
 
-class AnyCourses extends RequiredCourse {
+class AnyCourseIn extends RequiredCourse {
+  department: Department;
+  classifications: CourseClassification[];
+
+  constructor(department: Department, classifications: CourseClassification[]) {
+    super();
+    this.department = department;
+    this.classifications = classifications;
+  }
 }
 
 class Requirement {
-  department: Department;
-  classification: CourseClassification;
+  credit: number;
   requiredCourses: RequiredCourse[];
+
+  constructor(credit: number, requiredCourses: RequiredCourse[]) {
+    this.credit = credit;
+    this.requiredCourses = requiredCourses;
+  }
 }
+
+let exampleRequirementsForStudent = [
+  new Requirement(7, [
+    new SpecificCourse("HSS022"),
+    new SpecificCourse("HSS023"),
+    new SpecificCourse("HSS024"),
+    new SpecificCourse("HSS025"),
+    new ChooseNum(1, ["HSS001", "HSS002", "HSS003", "HSS004"]),
+  ]),
+  new Requirement(8, [
+    new ChooseNum(2, ["HSS040", "HSS041", "HSS042", "HSS043", "HSS044", "HSS045", "HSS046", "HSS047", "HSS048", "HSS049", "HSS050", "HSS051", "HSS052", "HSS053", "HSS054"]),
+    new ChooseNum(2, ["HSS060", "HSS061", "HSS062"]),
+    new SpecificCourse("HSS090"),
+    new SpecificCourse("HSS091"),
+  ]),
+  new Requirement(12, [
+    new AnyCourseIn(null, ["인문사회선택"]),
+  ]),
+  new Requirement(23, [
+    new ChooseNum(1, ["PH121", "PH141", "PH161"]),
+    new ChooseNum(1, ["PH122", "PH142", "PH162"]),
+    new SpecificCourse("PH151"),
+    new ChooseNum(1, ["BS110", "BS120"]),
+    new ChooseNum(1, ["MAS101", "MAS103"]),
+    new ChooseNum(1, ["MAS102", "MAS104"]),
+    new ChooseNum(1, ["CH100", "CH101", "CH105"]),
+    new ChooseNum(1, ["CH102", "CH106"]),
+    new ChooseNum(1, ["CS101", "CH102"]),
+  ]), // Missing PH171, PH172
+  new Requirement(3, [
+    new SpecificCourse("MAS109"),
+  ]),
+  new Requirement(19, [
+    new ChooseNum(1, ["CS204", "MAS275", "MA260"]),
+    new SpecificCourse("CS206"),
+    new SpecificCourse("CS300"),
+    new ChooseNum(1, ["CS311", "EE312"]),
+    new SpecificCourse("CS320"),
+    new SpecificCourse("CS330"),
+  ]),
+  new Requirement(30, [
+    new AnyCourseIn("전산학부", ["전공선택"]),
+  ]), // Missing Individual Studies
+  new Requirement(15, [
+    new ChooseNum(3, ["EE201", "EE202", "EE204", "EE209", "EE210", "EE211"]),
+    new SpecificCourse("EE305"),
+    new SpecificCourse("EE405"),
+  ]),
+  new Requirement(25, [
+    new AnyCourseIn("전기및전자공학부", ["전공선택", "전공필수"]),
+  ]), // Missing Individual Studies
+];
