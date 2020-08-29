a610adb79f83d1b26292059114a80a846d39ab9b
SeongYeop <appleminto55@gmail.com>
2020-08-29 13:53:42 +0900

    Raw excel data

--- calculate.js
+++ calculate.js
@@ -8,4 +8,20 @@ $(document).ready( function() {
         }
         output.innerHTML = '<ul>'+children+'</ul>';
     }
+
+    excelExport = function() {
+        alert("aaa");
+        var input = event.target;
+        var reader = new FileReader();
+        reader.onload = function(){
+            var fileData = reader.result;
+            var wb = XLSX.read(fileData, {type : 'binary'});
+            wb.SheetNames.forEach(function(sheetName){
+                var rowObj =XLSX.utils.sheet_to_json(wb.Sheets[sheetName]);
+                console.log(JSON.stringify(rowObj));
+            })
+        };
+        reader.readAsBinaryString(input.files[0]);
+}
+
 });
